814f923a7892d4727542fd593588740a136aa91b
Luiz Henrique <7henrique18@gmail.com>
2025-03-14 12:16:49 -0300

    refactor: remove ESLint configuration from docs and update package.json files to remove eslint-config dependency

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,7 @@
-import Image, { type ImageProps } from "next/image";
-
-
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Hello World</h1>
+		</div>
+	)
 }
