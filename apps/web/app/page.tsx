8b5b4780d6f0ffd8927f2814083798ca194a74e8
Luiz Henrique <7henrique18@gmail.com>
2025-03-14 12:53:04 -0300

    feat(web): integrate Tailwind CSS, add global styles, and update Home page layout

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from '@repo/ui/components/button'
 
 export default function Home() {
 	return (
-		<div className="flex flex-col items-center justify-center h-screen text-red-500">
-			<Button>Click me</Button>
+		<div className="flex flex-col items-center justify-center h-screen ">
+			<Button variant="outline">Click me</Button>
 		</div>
 	)
 }
