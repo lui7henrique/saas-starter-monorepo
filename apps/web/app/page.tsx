fde94d684f1adf5b7b0987de5be63fa504629f1f
Luiz Henrique <7henrique18@gmail.com>
2025-03-14 12:41:20 -0300

    feat(ui): update Home page to use Button component, enhance accordion component structure, and improve button component imports

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@repo/ui/components/button'
+
 export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center h-screen text-red-500">
-			<h1>Hello World</h1>
+			<Button>Click me</Button>
 		</div>
 	)
 }
