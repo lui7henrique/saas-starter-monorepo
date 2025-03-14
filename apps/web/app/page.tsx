dc6a5002a1b4e0648ebcb501b2e3e19e4ffebe7d
Luiz Henrique <7henrique18@gmail.com>
2025-03-14 15:37:24 -0300

    feat(web): add lucide-react icons to Button components in Home page and update package.json dependencies

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,107 @@
 import { Button } from '@repo/ui/components/button'
+import { AudioLines, Mic } from 'lucide-react'
 
 export default function Home() {
 	return (
-		<div className="flex gap-2 items-center justify-center h-screen">
-			<Button variant="outline">Click me</Button>
-			<Button variant="default">Click me</Button>
-			<Button variant="secondary">Click me</Button>
-			<Button variant="ghost">Click me</Button>
-			<Button variant="link">Click me</Button>
+		<div className="flex items-center justify-center h-screen gap-2">
+			<div className="flex flex-col gap-2 ">
+				<Button variant="outline">
+					<div className="flex items-center gap-2 p-1 rounded-full border">
+						<Mic className="size-3" />
+					</div>
+					Record audio
+				</Button>
+				<Button variant="default">
+					<div className="flex items-center gap-2 p-1 rounded-full">
+						<Mic className="size-3" />
+					</div>
+					Record audio
+				</Button>
+				<Button variant="secondary">
+					<div className="flex items-center gap-2 p-1 rounded-full border">
+						<Mic className="size-3" />
+					</div>
+					Record audio
+				</Button>
+				<Button variant="ghost">
+					<div className="flex items-center gap-2 p-1 rounded-full border">
+						<Mic className="size-3" />
+					</div>
+					Record audio
+				</Button>
+				<Button variant="link">
+					<div className="flex items-center gap-2 p-1 rounded-full border">
+						<Mic className="size-3" />
+					</div>
+					Record audio
+				</Button>
+			</div>
+
+			<div className="flex flex-col gap-2 ">
+				<Button variant="outline">
+					<div className="flex items-center gap-2 p-1 rounded-full border bg-red-500 text-white border-red-400">
+						<Mic className="size-3" />
+					</div>
+					Record audio
+				</Button>
+				<Button variant="default">
+					<div className="flex items-center gap-2 p-1 rounded-full bg-red-500 text-white border-red-400">
+						<Mic className="size-3" />
+					</div>
+					Record audio
+				</Button>
+				<Button variant="secondary">
+					<div className="flex items-center gap-2 p-1 rounded-full border bg-red-500 text-white border-red-400">
+						<Mic className="size-3" />
+					</div>
+					Record audio
+				</Button>
+				<Button variant="ghost">
+					<div className="flex items-center gap-2 p-1 rounded-full border bg-red-500 text-white border-red-400">
+						<Mic className="size-3" />
+					</div>
+					Record audio
+				</Button>
+				<Button variant="link">
+					<div className="flex items-center gap-2 p-1 rounded-full border bg-red-500 text-white border-red-400">
+						<Mic className="size-3" />
+					</div>
+					Record audio
+				</Button>
+			</div>
+
+			<div className="flex flex-col gap-2 ">
+				<Button variant="outline">
+					<div className="flex items-center gap-2 p-1 rounded-full border bg-red-500 text-white border-red-400">
+						<AudioLines className="size-3" />
+					</div>
+					Record audio
+				</Button>
+				<Button variant="default">
+					<div className="flex items-center gap-2 p-1 rounded-full bg-red-500 text-white border-red-400">
+						<AudioLines className="size-3" />
+					</div>
+					Record audio
+				</Button>
+				<Button variant="secondary">
+					<div className="flex items-center gap-2 p-1 rounded-full border bg-red-500 text-white border-red-400">
+						<AudioLines className="size-3" />
+					</div>
+					Record audio
+				</Button>
+				<Button variant="ghost">
+					<div className="flex items-center gap-2 p-1 rounded-full border bg-red-500 text-white border-red-400">
+						<AudioLines className="size-3" />
+					</div>
+					Record audio
+				</Button>
+				<Button variant="link">
+					<div className="flex items-center gap-2 p-1 rounded-full border bg-red-500 text-white border-red-400">
+						<AudioLines className="size-3" />
+					</div>
+					Record audio
+				</Button>
+			</div>
 		</div>
 	)
 }
